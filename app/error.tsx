0dff832feb12b96474265bfaa11545d7f1fe7d8c
Seung Hun Ko <seung.h.ko@outlook.com>
2023-07-06 16:30:35 -0400

    Add error page

---
+++ app/error.tsx
@@ -0,0 +1,7 @@
+export default function Error() {
+    return (
+        <div className="flex flex-col w-full justify-center items-center h-screen text-neutral-100">
+            Error
+        </div>
+    )
+}
--- app/match/error.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export default function Error() {
-    return (
-        <div className="flex flex-col w-full justify-center items-center h-screen">
-
-        </div>
-    )
-}
